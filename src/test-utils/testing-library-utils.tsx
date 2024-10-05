a711dec78a9d15f1f0ead40d1e13edc1789621e6
PDochev <plamendochev0@gmail.com>
2024-10-05 15:15:20 +0100

    Added Custom Testing render

---
+++ src/test-utils/testing-library-utils.tsx
@@ -0,0 +1,20 @@
+import { Provider } from "react-redux";
+import { RenderOptions, RenderResult, render } from "@testing-library/react";
+import store from "../store";
+
+import { ReactNode, ReactElement } from "react";
+
+const AllTheProviders = ({ children }: { children: ReactNode }) => {
+  return <Provider store={store}>{children}</Provider>;
+};
+
+const renderWithReduxStore = (
+  ui: ReactElement,
+  options?: RenderOptions
+): RenderResult => render(ui, { wrapper: AllTheProviders, ...options });
+
+// re-export everything
+export * from "@testing-library/react";
+
+// override render method
+export { renderWithReduxStore as render };
